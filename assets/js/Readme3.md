8967675367f8408889fa1d9eb9bd3e35fe715f6b
Marito <mtorob@unibarranquilla.edu.co>
2026-03-19 14:47:10 -0500

    Update and rename Readme3.md to assets.js

--- assets/js/Readme3.md
+++
@@ -1,1 +0,0 @@
-mefe biuyehdjdsh
---
+++ assets/js/assets.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aseet</title>
+</head>
+<body>
+    <h1>Postres Favoritos</h1><br>
+    <h2>El postre favorito de Maria</h2>
+    <img src="pastel de chocolate.avif" alt="pastel de chocolate"> 
+    <h2>El postre favorito de Maritrini</h2>
+    <img src="pie de limon.webp" alt="pie de limon"> 
+    <h2>El postre favorito de Valentina</h2>
+    <img src="pastel de tres leche.webp" alt="pastel de tres leches"> 
+    <h2>El postre favorito de Alejandra</h2>
+    <img src="churros.webp" alt="churros"> 
+</body>
+</html>
